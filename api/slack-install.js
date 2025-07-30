6bdf6041830feca27eb0b0fab2b228a38ab039ce
Ethan Neuman <ethanneuman@mac.lan>
2025-07-30 17:27:37 +0300

    Update files

--- api/slack-install.js
+++ api/slack-install.js
@@ -1,1 +1,10 @@
-dasdasdsadd
+export default function handler(req, res) {
+  const clientId = process.env.SLACK_CLIENT_ID;
+  const redirectUri = encodeURIComponent(process.env.SLACK_OAUTH_REDIRECT_URI);
+  const scopes = "commands,chat:write,app_mentions:read";
+  if (!clientId || !redirectUri) {
+    return res.status(500).send("Missing SLACK_CLIENT_ID or SLACK_OAUTH_REDIRECT_URI");
+  }
+  const url = `https://slack.com/oauth/v2/authorize?client_id=${clientId}&scope=${scopes}&redirect_uri=${redirectUri}`;
+  res.redirect(url);
+}
