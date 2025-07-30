4de60d41abc46f3c3561d9117e5c1a8b119c29fd
Ethan Neuman <ethanneuman@mac.lan>
2025-07-30 17:44:03 +0300

    Update files

--- pages/api/slack-events.js
+++ pages/api/slack-events.js
@@ -22,7 +22,6 @@ export default async function handler(req, res) {
     }
 
     if (data && data.challenge) {
-      // Réponse attendue exactement par Slack
       return res.status(200).json({ challenge: data.challenge });
     }
 
